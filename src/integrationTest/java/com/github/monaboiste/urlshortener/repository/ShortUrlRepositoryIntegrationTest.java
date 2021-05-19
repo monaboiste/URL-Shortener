d38aea4e0ea37e8a50295da36811d311cacd00fd
Monika Szumna <monikaszumna4@gmail.com>
2021-05-19 10:38:45 +0200

    Rewrited ShortUrlRepositoryIntegrationTest's tests

--- src/integrationTest/java/com/github/monaboiste/urlshortener/repository/ShortUrlRepositoryIntegrationTest.java
+++ src/integrationTest/java/com/github/monaboiste/urlshortener/repository/ShortUrlRepositoryIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.github.monaboiste.urlshortener.repository;
 
+import com.github.monaboiste.urlshortener.persistence.entity.ShortUrl;
 import com.github.monaboiste.urlshortener.persistence.repository.ShortUrlRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +8,13 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("integration")
 @DataJpaTest
-@Sql("/test-data.sql")
 public class ShortUrlRepositoryIntegrationTest {
 
     @Autowired
@@ -20,6 +23,12 @@ public class ShortUrlRepositoryIntegrationTest {
     @Test
     void shouldCheck_whenExistsByAlias() {
         final String validAlias = "sample-1";
+        final ShortUrl shortUrlWithValidAlias = ShortUrl.builder()
+                .url("example.com")
+                .alias(validAlias)
+                .createdAt(OffsetDateTime.of(2021, 5, 1, 10, 0 ,0 ,0 , ZoneOffset.UTC))
+                .build();
+        shortUrlRepository.save(shortUrlWithValidAlias);
 
         boolean existsByAlias = shortUrlRepository.existsByAlias(validAlias);
 
--- src/integrationTest/resources/test-data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO short_urls (id, alias, created_at, url)
-VALUES (1, 'sample-1', '2021-05-17 10:00:00', 'www1.example.com');
-INSERT INTO short_urls (id, alias, created_at, url)
-VALUES (2, 'sample-2', '2021-05-18 11:00:00', 'www2.example.com');
-INSERT INTO short_urls (id, alias, created_at, url)
-VALUES (3, 'sample-3', '2021-05-19 12:00:00', 'www3.example.com');
